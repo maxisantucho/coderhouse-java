54c76f693cf11a7f6e9ebd481362ec72baa9fff6
Maxi Santucho <maximilianosantucho@outlook.es>
2024-03-10 20:16:47 -0300

    Primer commit

---
+++ tarea/src/main/java/com/coderhouse/Main.java
@@ -0,0 +1,15 @@
+package com.coderhouse;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String mensaje;
+		
+		mensaje = "Hola mundo";
+		
+		System.out.println(mensaje);
+
+	}
+
+}
---
+++ tarea/src/main/java/com/coderhouse/Persona.java
@@ -0,0 +1,8 @@
+package com.coderhouse;
+
+public class Persona {
+	
+	String nombre;
+	String apellido;
+
+}
